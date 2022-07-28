bdcbe1296e1f98d0c23707cad71850b3dbacc576
kwnad <risnadiaaa@gmail.com>
2022-07-28 08:56:37 +0700

    Migration Siswa

---
+++ database/migrations/2022_07_28_015137_create_siswas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('siswas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nis')->unique();
+            $table->string('nama');
+            $table->unsignedBigInteger('id_kelas');
+            // membuat fk id_kelas yang mengacu kpd field id di table
+            // kelas
+            $table->foreign('id_kelas')->references('id')->on('kelas')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('siswas');
+    }
+};
